398628ae0d7f5acee5e2d4b978c5eef75f567017
Nate Suver <nate.suver@gmail.com>
2018-12-09 19:16:10 -0500

    code optimizations and test cases added

--- mapreduce/map.py
+++ mapreduce/map.py
@@ -10,11 +10,14 @@ def main():
         words = line.split()
         #--- output tuples [word, 1] in tab-delimited format---
         for word in words:
-            result=re.sub("[^A-Za-z|'|(-)]","", word) #remove any characters from the input that are not the letters a-z, single quote, or hyphen
-            result=scrubSequence(word,"'") #scrub unnecessary quotes
-            result=scrubSequence(word,"--") #scrub unnecessary double hyphens
+            result=re.sub("[^A-Za-z|'|-]","", word) #remove any characters from the input that are not the letters a-z, single quote, or hyphen
+            result=scrubSequence(result,"'") #scrub unnecessary quotes
+            result=scrubSequence(result,"--") #scrub unnecessary double hyphens
             result=re.sub("(?<=[A-Za-z])-(?=[A-Za-z])", "",result) #remove any additional hyphens where the preceding character is a letter, and the subsequent character is also a letter.  This handles the case of single-hyphenated words that would not have been picked up with previous methods
-            print('%s\t%s' %(result, "1")) #return the result standard out
+            result=re.sub("^-", "",result) #remove single hyphens at the start of the word
+            result=re.sub("-$", "",result) #remove single hyphens at the end of the word
+            if len(result)>0:
+                print('%s\t%s' %(result, "1")) #return the result standard out
       
 #Given a target word, and sequence to operate against, does the following:
 #Remove any instances of the target sequence that occur at the start of the word
